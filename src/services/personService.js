859555e355adea463fb64272948f35023cc71919
Mae <funj451@gmail.com>
2023-05-30 13:11:31 +0700

    first commit

---
+++ src/services/personService.js
@@ -0,0 +1,31 @@
+const baseUrl = "https://phonebook-app-api.onrender.com/api/persons";
+
+function getPersons() {
+  return fetch(baseUrl)
+    .then((res) => res.json())
+    .then((data) => data);
+}
+
+function createPerson(person) {
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(person),
+  })
+    .then((res) => res.json())
+    .then((data) => data);
+}
+
+function deletePerson(id) {
+  return fetch(`${baseUrl}/${id}`, {
+    method: "DELETE",
+  }).then((res) => res.status);
+}
+
+export default {
+  getPersons,
+  createPerson,
+  deletePerson,
+};
